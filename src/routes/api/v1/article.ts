31abf6d31ca1b01e4e730eeaf3b72a65ef5a9642
Vitalii Shebela <v.shebela@crossroadlabs.xyz>
2017-12-21 19:18:59 +0200

    Fixed pagination

--- src/routes/api/v1/article.ts
+++ src/routes/api/v1/article.ts
@@ -1,11 +1,23 @@
 import * as express from 'express';
 import { Router } from 'express-serve-static-core';
+
+import Article from '../../../models/Article';
 import { isLoggedIn } from './helpers';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send('article');
+  const page = parseInt(req.query.page, 10);
+  const limit = parseInt(req.query.limit, 10);
+  const query = req.query.userId === 'null' ? {} : { userId: req.query.userId };
+  if (page < 1) { res.status(404).send(); }
+  Article.find(query).skip((page - 1) * limit).limit(limit)
+    .then((results) => {
+      return results.length ? res.status(200).json(results) : res.status(404).send();
+    })
+    .catch((err) => {
+      return res.status(403);
+    });
 });
 
 export default router;
