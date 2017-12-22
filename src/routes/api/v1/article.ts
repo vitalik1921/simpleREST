4473e1d8eda14a2703395992249aaed695b152e9
Vitalii Shebela <v.shebela@crossroadlabs.xyz>
2017-12-22 11:53:02 +0200

    Added single article route

--- src/routes/api/v1/article.ts
+++ src/routes/api/v1/article.ts
@@ -6,18 +6,33 @@ import { isLoggedIn } from './helpers';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const page = parseInt(req.query.page, 10);
-  const limit = parseInt(req.query.limit, 10);
-  const query = req.query.userId === 'null' ? {} : { userId: req.query.userId };
-  if (page < 1) { res.status(404).send(); }
-  Article.find(query).skip((page - 1) * limit).limit(limit)
-    .then((results) => {
-      return results.length ? res.status(200).json(results) : res.status(404).send();
+router.route('/:id')
+  .get((req, res) => {
+    Article.findById(req.params['id']).then((article) => {
+      return res.json(article).status(200);
     })
     .catch((err) => {
-      return res.status(403);
+      console.log(err);
+      return res.sendStatus(404);
     });
-});
+  })
+  .post((req, res) => {
+
+  });
+
+router.route('/')
+  .get((req, res) => {
+    const page = parseInt(req.query.page, 10);
+    const limit = parseInt(req.query.limit, 10);
+    const query = req.query.userId === 'null' ? {} : { userId: req.query.userId };
+    if (page < 1) { res.status(404).send(); }
+    Article.find(query).skip((page - 1) * limit).limit(limit)
+      .then((results) => {
+        return results.length ? res.status(200).json(results) : res.status(404).send();
+      })
+      .catch((err) => {
+        return res.status(403);
+      });
+  });
 
 export default router;
